d273795a99f99af3cfddaa9faee4137bdc4be2a4
Akash Sethi <as924u@att.com>
2017-12-25 13:12:23 +0530

    Updated appModule for registering services and module dependency and added app layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
+import {AppComponent} from './app.component';
+import {
+  HeaderComponent,
+  FooterComponent,
+  JwtService,
+  UserService,
+  ApiService,
+  SharedModule,
+  AlertComponent,
+  AlertService
+} from './shared';
+import {RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-
+const rootRoutes = RouterModule.forRoot([]);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    rootRoutes,
+    HttpClientModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    JwtService,
+    UserService,
+    ApiService,
+    AlertService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
